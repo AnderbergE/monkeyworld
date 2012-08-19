079efbc3d094a69a0303958e76c644662493f77d
Björn Norrliden <bjorn.norrliden@gmail.com>
2012-08-19 20:34:16 +0200

    Temporarily use mouse graphics in MonkeyAgentView

--- src/view/Agents/MonkeyAgentView.js
+++ src/view/Agents/MonkeyAgentView.js
@@ -4,9 +4,14 @@
  */
 MW.MonkeyAgentView = function() {
 	MW.AgentView.call(this);
-	this.normalFace = function () { return MW.Images.MONKEY; };
-	this.happyFace = function () { return MW.Images.MONKEY; };
-	this.standing = function () { return MW.Images.MONKEY; };
+	MW.AgentView.call(this);
+	this.bodyOffset = function () { return { x: 0, y: 0 }; };
+	this.feetOffset = function () { return 315; };
+	this.faceOffset = function () { return { x: -12, y: -11 }; };
+	this.head = function () { return MW.Images.AGENT_MOUSE_HEAD; };
+	this.standing = function () { return MW.Images.AGENT_MOUSE_NORMAL; };
+	this.normalFace = function () { return MW.Images.AGENT_FACE_NORMAL_MOUSE; };
+	this.happyFace = function () { return MW.Images.AGENT_FACE_HAPPY_MOUSE; };
 };
 
 /**
