36baa4cd5942fcd4ab2b7f9a38ffe2bb7dbbf608
Björn Norrliden <bjorn.norrliden@gmail.com>
2012-08-23 11:56:49 +0200

    Adds dancing agents (only mice so far) to IntroductionView

--- src/view/Game/IntroductionView.js
+++ src/view/Game/IntroductionView.js
@@ -14,7 +14,29 @@ MW.IntroductionView = function (callback) {
 		layer = new Kinetic.Layer();
 		stage.add(layer);
 		
-		//layer.add(new Kinetic.Rect({width:200,height:200,fill:"blue"}));
+		var mouseView = new MW.MouseAgentView();
+		var mouse = mouseView.getBody(view, 100, 200);
+		layer.add(mouse);
+		mouseView.dance();
+		mouse.setScale(0.7);
+		
+		var mouseView2 = new MW.MouseAgentView();
+		var mouse2 = mouseView2.getBody(view, 300, 200);
+		layer.add(mouse2);
+		mouseView2.dance();
+		mouse2.setScale(0.7);
+		
+		var mouseView3 = new MW.MouseAgentView();
+		var mouse3 = mouseView3.getBody(view, 500, 200);
+		layer.add(mouse3);
+		mouseView3.dance();
+		mouse3.setScale(0.7);
+
+		var mouseView4 = new MW.MouseAgentView();
+		var mouse4 = mouseView4.getBody(view, 700, 200);
+		layer.add(mouse4);
+		mouseView4.dance();
+		mouse4.setScale(0.7);	
 
 		view.setTimeout(function () { MW.Sound.play(MW.Sounds.INTRO_1); }, 2000);
 		view.setTimeout(function () { MW.Sound.play(MW.Sounds.INTRO_2); }, 2000);
